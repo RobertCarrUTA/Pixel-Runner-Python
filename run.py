3a35185739508332713955cabb88abb6ece5d650
Robert Carr <robertcarrcisd@gmail.com>
2022-08-19 19:39:47 -0500

    Window is now titled and has fps cap of 60

--- run.py
+++ run.py
@@ -5,7 +5,11 @@ from sys import exit # Secure way to end the program
 
 # pygame.init() - starts pygame and initiates all the sub parts of pygame
 pygame.init()
-screen = pygame.display.set_mode((800, 400)) # width of 800 pixels, height of 400 pixels
+screen = pygame.display.set_mode((800, 400)) # Width of 800 pixels, height of 400 pixels
+pygame.display.set_caption("Run") # Sets the title of the window
+
+# A Clock object is used to keep track of time and manage the framerate
+clock = pygame.time.Clock()
 
 while True:
     # We need to check for all the possible types of player input
@@ -14,6 +18,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    
 
     pygame.display.update()
+
+    clock.tick(60) # Tells pygame it should never run faster than 60 fps
