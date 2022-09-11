6f080fe84ecf93e759546500e80f84978ce6a957
Robert Carr <robertcarrcisd@gmail.com>
2022-09-11 15:00:12 -0500

    Time is now displayed in seconds on the screen
    The user can now see how many seconds they have lasted without being hit.

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
 # This code could be heavily commented because it is for me to learn, so I will take notes
 
 import pygame
-from sys import exit # Secure way to end the program
+from sys import exit    # Secure way to end the program
+import math             # For rounding seconds
+
+def display_score():
+    # pygame.time.get_ticks() will give us a time in milliseconds since we called pygame.init
+    # We need to subtract the time since we last restarted to reset the timer to 0 every restart
+    current_time_milliseconds = pygame.time.get_ticks() - start_time
+    current_time = math.floor(current_time_milliseconds/1000)
+    
+    score_surface = test_font.render("Score: " + str(current_time), True, "Black") # Arguments: (text, AA, color) - AA - anti-alias option
+    score_rect = score_surface.get_rect(center = (400, 50))
+    screen.blit(score_surface, score_rect)
+
 
 # pygame.init() - starts pygame and initiates all the sub parts of pygame
 pygame.init()
@@ -18,9 +30,6 @@ ground_surface = pygame.image.load("graphics/ground.png").convert()
 snail_surface = pygame.image.load("graphics\snail\snail1.png").convert_alpha()
 player_surface = pygame.image.load("graphics\player\player_walk_1.png").convert_alpha()
 
-score_surface = test_font.render("Score: ", True, "Black") # Arguments: (text, AA, color) - AA - anti-alias option
-score_rect = score_surface.get_rect(center = (400, 50))
-
 end_surface = test_font.render("Press Space to play again", True, "Black") # Arguments: (text, AA, color) - AA - anti-alias option
 end_rect = end_surface.get_rect(center = (400, 200))
 
@@ -32,6 +41,7 @@ snail_rect = snail_surface.get_rect(midbottom = (600, 300))
 player_gravity = 0
 
 game_active = True
+start_time = 0 # Keep track of our time
 
 while True:
     # We need to check for all the possible types of player input
@@ -53,6 +63,9 @@ while True:
                 snail_rect.left = 800
                 game_active = True
 
+                # Lets us restart our time every time we restart
+                start_time = pygame.time.get_ticks()
+
     if game_active:
         # Attach the test surface to the display surface
         # .blit() stands for block image transfer
@@ -62,7 +75,9 @@ while True:
         #   and 100 pixels from the top
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300)) # 300 because that is when the sky_surface image ends
-        screen.blit(score_surface, score_rect)
+        
+        #screen.blit(score_surface, score_rect)
+        display_score()
 
         # Snail
         # snail_rect.x is updated in the loop to animate the snail moving towards the player
