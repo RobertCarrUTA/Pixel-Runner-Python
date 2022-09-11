23ba5ea764bcea38a75b7f5edf1ef2925f71758a
Robert Carr <robertcarrcisd@gmail.com>
2022-09-10 22:05:03 -0500

    The player can now jump when pressing space
    The player can now jump when pressing space on the keyboard. It will not fall below the floor and it will only jump if the player is touching the floor.

--- run.py
+++ run.py
@@ -17,12 +17,16 @@ sky_surface = pygame.image.load("graphics/sky.png").convert()
 ground_surface = pygame.image.load("graphics/ground.png").convert()
 snail_surface = pygame.image.load("graphics\snail\snail1.png").convert_alpha()
 player_surface = pygame.image.load("graphics\player\player_walk_1.png").convert_alpha()
-text_surface = test_font.render("Score: ", True, "Black") # Arguments: (text, AA, color) - AA - anti-alias option
+
+score_surface = test_font.render("Score: ", True, "Black") # Arguments: (text, AA, color) - AA - anti-alias option
+score_rect = score_surface.get_rect(center = (400, 50))
 
 # Creating a player rectangle to gain more control over positioning as opposed to a surface
 # .get_rect() gets the surface and draws a rectangle around it
 player_rect = player_surface.get_rect(midbottom = (80, 300))
 snail_rect = snail_surface.get_rect(midbottom = (600, 300))
+
+player_gravity = 0
 
 while True:
     # We need to check for all the possible types of player input
@@ -31,6 +35,12 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.KEYDOWN:
+            # When the player jumps using SPACE
+            if event.key == pygame.K_SPACE:
+                # Only allow the player to jump if they are touching the ground
+                if player_rect.bottom == 300:
+                    player_gravity = -20
 
     # Attach the test surface to the display surface
     # .blit() stands for block image transfer
@@ -40,17 +50,24 @@ while True:
     #   and 100 pixels from the top
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300)) # 300 because that is when the sky_surface image ends
-    screen.blit(text_surface, (320, 30))
-    screen.blit(snail_surface, snail_rect)
+    screen.blit(score_surface, score_rect)
 
+    # Snail
     # snail_rect.x is updated in the loop to animate the snail moving towards the player
     snail_rect.x -= 4
     if snail_rect.right <= 0:
         snail_rect.left = 800
-    
+    screen.blit(snail_surface, snail_rect)
+
+    # Player
     # I would like to note that you can print the value of a rectangle. Example:
     # print(player_rect.left)
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 300:
+        player_rect.bottom = 300
     screen.blit(player_surface, player_rect)
+    
 
     pygame.display.update()
 
